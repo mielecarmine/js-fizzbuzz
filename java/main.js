c3ee6c00bcd25a54e23d306ed584bb214d5033a6
mielecarmine <mielecarmine993@gmail.com>
2024-01-11 16:51:16 +0100

    feat: css

--- java/main.js
+++ java/main.js
@@ -9,15 +9,18 @@ const boxContainerElement = document.getElementById("box-container");
 
 for (let i = 1; i <= 100; i++) {
   const boxElement = document.createElement("div");
-  boxElement.className = "card p-2 ";
+  boxElement.className = "box p-2 text-center";
 
   if (i % 15 == 0) {
+    boxElement.classList.add("fizzbuzz");
     boxElement.innerText = "FizzBuzz";
     console.log("FizzBuzz");
   } else if (i % 5 == 0) {
+    boxElement.classList.add("buzz");
     boxElement.innerText = "Buzz";
     console.log("Buzz");
   } else if (i % 3 == 0) {
+    boxElement.classList.add("fizz");
     boxElement.innerText = "Fizz";
     console.log("Fizz");
   } else {
